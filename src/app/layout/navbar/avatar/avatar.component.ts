e4d544507a465811a7c42996a37ba8d4855dd778
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-09-15 13:52:59 +0300

    Implemented Avatar component

--- src/app/layout/navbar/avatar/avatar.component.ts
+++ src/app/layout/navbar/avatar/avatar.component.ts
@@ -1,16 +1,46 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-avatar',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+    FontAwesomeModule
+  ],
   template: `
-    <p>
-      avatar works!
-    </p>
+  <div class="flex align-content-center">
+    @if(imageUrl() != null) {
+      <img src="{{imageUrl()}}" alt="user-avatar" class="border-round-3xl avatar"
+          [ngClass]="avatarSize()">
+    } @else {
+      <div class="flex align-items-center justify-content-center unknow-user"
+      [ngClass]="avatarSize()">
+        <fa-icon [icon]="'user'" class="text-dark"></fa-icon>
+      </div>
+    }
+  </div>
   `,
-  styles: ``
+  styles: `
+  .avatar {
+    object-fit: contain;
+  }
+
+  .avatar-sm {
+    width: 35.75px;
+    height: 35.75px;
+  }
+
+  .avatar-xl {
+    width: 50px;
+    height: 50px;
+  }
+  `
 })
 export class AvatarComponent {
+
+  imageUrl = input<string>();
+  avatarSize = input<"avatar-sm" | "avatar-xl">();
 
 }
