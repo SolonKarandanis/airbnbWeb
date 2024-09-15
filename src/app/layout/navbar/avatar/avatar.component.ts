bf50082f6333108b891ae386f0d0890295726bab
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-09-15 11:03:11 +0300

    Created layout components and services

---
+++ src/app/layout/navbar/avatar/avatar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-avatar',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      avatar works!
+    </p>
+  `,
+  styles: ``
+})
+export class AvatarComponent {
+
+}
