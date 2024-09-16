66ebafe1ec3355f7d9120af4d9a46ecebde0ef1d
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-09-16 14:50:48 +0300

    Implemented Footer component

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,15 +1,37 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   template: `
-    <p>
-      footer works!
-    </p>
+  <div class="footer text-sm border-solid border-1 border-200 flex
+    justify-content-start align-items-center px-6 py-5">
+    <span>© 2024 Codecake, Inc.</span>
+    <fa-icon icon="circle"></fa-icon>
+    <span>Terms</span>
+    <fa-icon icon="circle"></fa-icon>
+    <span>Sitemap</span>
+    <fa-icon icon="circle"></fa-icon>
+    <span>Privacy</span>
+    <fa-icon icon="circle"></fa-icon>
+    <span>Your privacy choices</span>
+  </div>
   `,
-  styles: ``
+  styles: `
+  @import "./../../../assets/scss/airbnb-variables";
+  
+  .footer {
+    background-color: $footerBgColor;
+  }
+
+  fa-icon {
+    margin-left: 10px;
+    font-size: 3px;
+    margin-right: 10px;
+  }
+  `
 })
 export class FooterComponent {
 
