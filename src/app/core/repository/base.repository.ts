ec723bbb2568d9b7a90a5302d02f5a47d25ada63
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-09-30 21:36:54 +0300

    Create BaseRepository

---
+++ src/app/core/repository/base.repository.ts
@@ -0,0 +1,9 @@
+import { environment } from "src/environments/environment";
+
+export class BaseRepository{
+    private readonly _airBnbEndpoint = `http://${environment.airbnbDomain}/`;
+
+    public get airBnbEndpoint(): string{
+        return this._airBnbEndpoint;
+    }
+}
