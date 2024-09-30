396c1b260c04abfe742e053469b1743860d3e161
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-09-30 22:22:21 +0300

    Implemented FilesRepository

---
+++ src/app/core/repository/ApiControllers.ts
@@ -0,0 +1,8 @@
+export enum ApiControllers {
+    AUTH='authenticate',
+    USERS = 'users',
+    SEARCH = 'search',
+    SAVED_SEARCH = 'savedSearches',
+    SEARCH_CONFIGURATION='searchConfiguration',
+    FILES='files',
+}
--- src/app/core/repository/file.repository.ts
+++ src/app/core/repository/file.repository.ts
@@ -1,14 +1,42 @@
 import { Observable } from "rxjs";
 import { BaseRepository } from "./base.repository";
-import { HttpResponse } from "@angular/common/http";
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { ApiControllers } from "./ApiControllers";
 
-export abstract class FilesRepository extends BaseRepository{
+@Injectable({
+    providedIn: 'root'
+})
+export class FilesRepository extends BaseRepository{
+
+    private filesEndpoint = `${this.airBnbEndpoint}/${ApiControllers.FILES}`;
+
+    private http= inject(HttpClient);
+    private httpUtil= inject(HttpUtil);
 
     /**
      * Get the data of a file from the backend for downloading
      * @param fileid The file's ID
      * @returns An observable with the file's data in ArrayBuffer
      */
-    abstract downloadFile(fileid: number): Observable<HttpResponse<ArrayBuffer>>
+   
+    
+    downloadFile(fileid: number): Observable<HttpResponse<ArrayBuffer>> {
+        return this.http.get(`${this.filesEndpoint}/${fileid}`, {
+            responseType: 'arraybuffer',
+            observe: 'response',
+        });
+    }
+
+    // downloadFile(fileid: number): Observable<GenericFile> {
+    //     return this.filesController.downloadFile(fileid).pipe(
+    //         map((responseData: HttpResponse<ArrayBuffer>) => ({
+    //             filename: this.httpUtil.getFileNameForContentDisposition(responseData.headers),
+    //             id: fileid,
+    //             mimeType: responseData.headers.get('Content-Type')!,
+    //             arrayBuffer: responseData.body!,
+    //         }))
+    //     );
+    // }
     
 }
