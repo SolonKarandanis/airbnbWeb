e0b066a3eca65292ad197ffab622b6333181876a
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-09-30 21:32:17 +0300

    Created environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  webApiEndpointLocal:'http://localhost:4200',
+
+  airbnbDomain:'localhost:8080',
+  appVersion: 'v8.0.27',
+  USERDATA_KEY: 'authf649fc9a5f55',
+  isMockEnabled: true,
+}
