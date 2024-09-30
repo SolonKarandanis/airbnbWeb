848ebb223130cda3608528a86743771ddacdb7b1
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-09-30 17:53:26 +0300

    Created user and search models

---
+++ src/app/core/models/search.model.ts
@@ -0,0 +1,113 @@
+export type SearchResult<T>={
+    list:T[],
+    countRows:number
+  }
+  
+  export interface SearchTableColumn {
+    title: string;
+    field: string;
+    isLink?: boolean;
+    routerLinkConfig?: SearchTableColumnLinkRouterConf;
+    dataFieldForRoute?: string;
+    isCheckbox?: boolean;
+    isButton?: boolean;
+    icon?: string;
+    style?:string;
+    enableSorting?: boolean;
+    isDate?: boolean;
+    isImage?: boolean;
+    isTranslatable?:boolean;
+    onlyIcon?:boolean;
+    isStatus?:boolean;
+    isTableActions?:boolean;
+    toolTip?:string;
+    dataForToolTip?:string;
+    actions?:SearchTableColumnAction[];
+  }
+
+  export interface SearchTableColumnAction{
+    type:ActionTypes;
+    toolTip:string;
+    dataForToolTip?:string;
+    routerLinkConfig?: SearchTableColumnLinkRouterConf;
+    dataFieldForRoute?: string;
+    isLink?: boolean;
+    isButton?:boolean;
+    callbackFn?:(args?: any) => void;
+  }
+
+  export enum ActionTypes{
+    VIEW='VIEW',
+    EDIT='EDIT',
+    DELETE='DELETE'
+  }
+  
+  export interface SearchTableColumnLinkRouterConf {
+    preRoutes?: string[];
+    postRoutes?: string[];
+  }
+  
+  export enum SortDirection {
+    ASC = 'ASC',
+    DESC = 'DESC',
+  }
+  
+  export interface Paging {
+    page: number;
+    limit: number;
+    sortField?: string;
+    sortDirection?: SortDirection;
+  }
+
+  export interface SearchRequest {
+    paging: Paging;
+  }
+  
+  export interface UserSearchRequest extends SearchRequest {
+    username?: string;
+    firstName?: string;
+    email?: string;
+    status?: string;
+  }
+
+  export interface ListingSearchRequest extends SearchRequest{
+
+  }
+
+  export type SearchRequestCriteria = UserSearchRequest | ListingSearchRequest;
+  
+  export enum SearchTypeEnum {
+    ATC_VERSIONS = 'search.type.atcversions',
+    USERS = 'search.type.users',
+    PRODUCT_LIST_VERSION= 'search.type.product.list.version',
+    PRODUCT_LIST= 'search.type.product.list'
+  }
+
+
+  export interface SavedSearchModel {
+    id?: number;
+    username: string;
+    searchType: SearchTypeEnum;
+    savedSearchName: string;
+    criteria: SearchRequestCriteria;
+  }
+  
+  export interface SearchConfigurationCriteria {
+    fieldName: string;
+    searchable: boolean;
+    displayable: boolean;
+    customizable: boolean;
+    sortable?: boolean;
+    canFieldBeSearched: boolean;
+    canFieldBeDisplayed: boolean;
+  }
+  
+  export interface SearchConfigurationModel {
+    id?: number;
+    username: string;
+    searchType: SearchTypeEnum;
+    resultCount: number;
+    sortColumnKey?: string;
+    sortDirection?: SortDirection;
+    criteria: SearchConfigurationCriteria[];
+  }
---
+++ src/app/core/models/user.model.ts
@@ -0,0 +1,8 @@
+export interface UserModel{
+    username: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    status: string;
+    authorities:string[]
+}
