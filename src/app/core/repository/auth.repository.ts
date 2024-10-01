8b4a747995953dd5006ffb17c15990324923c35e
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-01 07:26:29 +0300

    Created Auth Repo

--- src/app/core/repository/auth.repository.ts
+++ src/app/core/repository/auth.repository.ts
@@ -2,9 +2,10 @@ import { inject, Injectable } from "@angular/core";
 import { BaseRepository } from "./base.repository";
 import { Observable } from "rxjs";
 import { UserModel } from "@models/user.model";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpContext } from "@angular/common/http";
 import { ApiControllers } from "./ApiControllers";
 import { JwtDTO, SubmitCredentialsDTO } from "@models/auth.model";
+import { AUTHENTICATE_REQUEST } from "../services/intercept.service";
 
 @Injectable({
     providedIn: 'root',
@@ -13,11 +14,23 @@ export class AuthRepository extends BaseRepository{
 
     private http = inject(HttpClient);
 
-    // login(credentials:SubmitCredentialsDTO):Observable<JwtDTO>{
-    //     return this.http.post<JwtDTO>()
-    // }
+    login(credentials:SubmitCredentialsDTO):Observable<JwtDTO>{
+        return this.http.post<JwtDTO>(`${this.airBnbEndpoint}/${ApiControllers.AUTH}`,credentials,{
+            context: new HttpContext().set(AUTHENTICATE_REQUEST, false),
+        })
+    }
 
     getUserByToken(): Observable<UserModel> {
         return this.http.get<UserModel>(`${this.airBnbEndpoint}/${ApiControllers.USERS}/account`);
     }
+
+    // getUserOperations(): Observable<OperationModel[]> {
+    //     return this.http.get<Operations[]>(`${this.dutEndpoint}/${ApiControllers.USERS}/operations`)
+    //         .pipe(
+    //             map(response =>{
+    //                 const models =response.map(this.operationMapper.mapToModel)
+    //                 return models;
+    //             })
+    //         )
+    // }
 }
