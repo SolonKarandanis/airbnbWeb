9da44f7cb21f0f4f296dc2f9d52f82ae7354a3da
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-01 14:42:49 +0300

    Added interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,8 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authExpired } from './core/services/auth-expired.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -10,5 +12,8 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes), 
     provideStore(),
     provideAnimations(),
+    provideHttpClient(
+      withInterceptors([authExpired]),
+    )
   ]
 };
