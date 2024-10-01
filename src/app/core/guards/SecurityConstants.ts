55821423db944313aa57385eab7a735bfb07e336
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-01 14:29:41 +0300

    Created RoleGuard

---
+++ src/app/core/guards/SecurityConstants.ts
@@ -0,0 +1,11 @@
+export enum RolesConstants{
+    ROLE_ADMIN = "ROLE_ADMIN",
+    ROLE_TENANT = "ROLE_TENANT",
+    ROLE_LANDLORD = "ROLE_LANDLORD",
+}
+
+export const userRoles =[
+    RolesConstants.ROLE_ADMIN,
+    RolesConstants.ROLE_TENANT,
+    RolesConstants.ROLE_LANDLORD,
+];
---
+++ src/app/core/guards/role.guard.ts
@@ -0,0 +1,31 @@
+import { AuthStore } from './../store/auth/auth-store';
+import { inject, Injectable, Signal } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, RouterStateSnapshot } from "@angular/router";
+import { MessageService } from "primeng/api";
+import { TranslateService } from '@ngx-translate/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class RoleGuard implements CanActivate {
+    private authStore = inject(AuthStore);
+    private messageService = inject(MessageService);
+    private translate = inject(TranslateService);
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {
+        const isAuthorized = this.isAuthorized(route);
+        if(!isAuthorized()){
+            this.messageService.add({
+                severity: 'error',
+                summary: this.translate.instant('GLOBAL.ERRORS.SUMMARY'),
+                detail: this.translate.instant('GLOBAL.ERRORS.FORBIDDEN'),
+            })
+        }
+        return isAuthorized();
+    }
+
+    private isAuthorized(route:ActivatedRouteSnapshot):Signal<boolean>{
+        const allowedRoles = route.data['allowedRoles'] as string[];
+        return this.authStore.hasAnyAuthority(allowedRoles);
+    }
+}
