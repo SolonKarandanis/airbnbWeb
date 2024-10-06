f37526f3d1b3d9605096b19ef9f6dc178e94a1b3
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-06 15:04:50 +0300

    Extracted protected routes to seperate file

--- src/app/protected/protected.module.ts
+++ src/app/protected/protected.module.ts
@@ -2,27 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
-import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { protectedRoutes } from './protected.routes';
 
-const childrenRoutes: Routes =[
-  {
-    path: 'home',
-    component:HomeComponent
-  },
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full',
-  },
-]
 
 const routes: Routes =[
   {
     path: '',
     component: LayoutComponent,
-    children:childrenRoutes
+    children:protectedRoutes
   },
 ]
 
