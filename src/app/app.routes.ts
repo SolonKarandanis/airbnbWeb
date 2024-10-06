fd327ad1eadc726b0745f7657efaaa6747195799
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-06 13:59:15 +0300

    Created Home Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'auth/login',
+        loadComponent: () => 
+            import('./auth/login/login.component').then((m)=>m.LoginComponent),
+    },
+    {
+        path:'auth/register',
+        loadComponent: () => 
+            import('./auth/register/register.component').then((m)=>m.RegisterComponent),
+    },
+];
