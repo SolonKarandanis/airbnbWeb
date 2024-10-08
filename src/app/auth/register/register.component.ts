7866ec11e8ceea70ea117b77d5ca11c66b310a66
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-08 07:56:08 +0300

    Implemented Registration page

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -5,71 +5,91 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <div class="container">
-      <div class="title">Registration</div>
-      <div class="content">
-        <form action="#">
-          <div class="user-details">
-            <div class="input-box">
-              <span class="details">Username</span>
-              <input type="text" placeholder="Enter your username" required>
-            </div>
-            <div class="input-box">
-              <span class="details">Password</span>
-              <input type="text" placeholder="Enter your password" required>
-            </div>
-            <div class="input-box">
-              <span class="details">Confirm Password</span>
-              <input type="text" placeholder="Confirm your password" required>
-            </div>
-            <div class="input-box">
-              <span class="details">Full Name</span>
-              <input type="text" placeholder="Enter your name" required>
-            </div>
-            <div class="input-box">
-              <span class="details">Email</span>
-              <input type="text" placeholder="Enter your email" required>
-            </div>
-            <div class="input-box">
-              <span class="details">Phone Number</span>
-              <input type="text" placeholder="Enter your number" required>
-            </div>
+    <div class="body">
+      <div class="container">
+          <div class="title">Registration</div>
+          <div class="content">
+            <form action="#">
+              <div class="user-details">
+                <div class="input-box">
+                  <span class="details">Username</span>
+                  <input type="text" placeholder="Enter your username" required>
+                </div>
+                <div class="input-box">
+                  <span class="details">Password</span>
+                  <input type="text" placeholder="Enter your password" required>
+                </div>
+                <div class="input-box">
+                  <span class="details">Confirm Password</span>
+                  <input type="text" placeholder="Confirm your password" required>
+                </div>
+                <div class="input-box">
+                  <span class="details">Full Name</span>
+                  <input type="text" placeholder="Enter your name" required>
+                </div>
+                <div class="input-box">
+                  <span class="details">Email</span>
+                  <input type="text" placeholder="Enter your email" required>
+                </div>
+                <div class="input-box">
+                  <span class="details">Phone Number</span>
+                  <input type="text" placeholder="Enter your number" required>
+                </div>
+              </div>
+              <div class="gender-details">
+                <input type="radio" name="gender" id="dot-1">
+                <input type="radio" name="gender" id="dot-2">
+                <input type="radio" name="gender" id="dot-3">
+                <span class="gender-title">Gender</span>
+                <div class="category">
+                  <label for="dot-1">
+                    <span class="dot one"></span>
+                    <span class="gender">Male</span>
+                  </label>
+                  <label for="dot-2">
+                    <span class="dot two"></span>
+                    <span class="gender">Female</span>
+                  </label>
+                  <label for="dot-3">
+                    <span class="dot three"></span>
+                    <span class="gender">Prefer not to say</span>
+                  </label>
+                </div>
+              </div>
+              <div class="button">
+                <input type="submit" value="Register">
+              </div>
+            </form>
           </div>
-          <div class="gender-details">
-            <input type="radio" name="gender" id="dot-1">
-            <input type="radio" name="gender" id="dot-2">
-            <input type="radio" name="gender" id="dot-3">
-            <span class="gender-title">Gender</span>
-            <div class="category">
-              <label for="dot-1">
-                <span class="dot one"></span>
-                <span class="gender">Male</span>
-              </label>
-              <label for="dot-2">
-                <span class="dot two"></span>
-                <span class="gender">Female</span>
-              </label>
-              <label for="dot-3">
-                <span class="dot three"></span>
-                <span class="gender">Prefer not to say</span>
-              </label>
-            </div>
-          </div>
-          <div class="button">
-            <input type="submit" value="Register">
-          </div>
-        </form>
       </div>
     </div>
   `,
   styles: `
+    .body{
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 10px;
+    }
     .container{
+      max-width: 700px;
+      width: 100%;
+      background-color: #fff;
+      padding: 25px 30px;
+      border-radius: 5px;
+      box-shadow: 0 5px 10px rgba(0, 0, 0, 0.15);
+    }
+
+    .content-container{
       display:flex;
+      flex-direction:column;
       align-items:center;
       justify-content: center;
-      min-height: 100vh;
       background-color: #fff;
-      
+      width: 50%;
+      height:50%;
+      padding: 1rem;
     }
 
     .container .title {
