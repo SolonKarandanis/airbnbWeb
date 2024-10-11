7497dbbfe9400f0c2d2681af8f4867f92e5e13f2
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-11 11:32:10 +0300

    Adding Tranlation interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authExpired } from './core/services/auth-expired.interceptor';
 import { MessageService } from 'primeng/api';
+import { TranslateService } from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader'
+import { firstValueFrom } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,6 +21,23 @@ export const appConfig: ApplicationConfig = {
       withInterceptors([
         authExpired
       ]),
-    )
+    ),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: appInitializerFactory,
+      deps: [TranslateService],
+      multi: true,
+    },
   ]
 };
+
+export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
+
+export function appInitializerFactory(translate: TranslateService) {
+  return (): Promise<any> => {
+      translate.setDefaultLang('en');
+      return firstValueFrom(translate.use('en'));
+  };
+}
