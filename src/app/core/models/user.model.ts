d425b7ad6ae373f71b25c7b92dce8f96cf48cbc8
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-15 13:05:52 +0300

    Created User models

--- src/app/core/models/user.model.ts
+++ src/app/core/models/user.model.ts
@@ -1,8 +1,21 @@
-export interface UserModel{
+export interface BaseUserModel{
     username: string;
     firstName: string;
     lastName: string;
     email: string;
+}
+
+export interface UserModel extends BaseUserModel{
+    imageUrl: string;
     status: string;
     authorities:string[]
+}
+
+export interface CreateUserRequest extends BaseUserModel{
+    password:string;
+    role:string;
+}
+
+export interface UpdateUserRequest{
+    role:string;
 }
