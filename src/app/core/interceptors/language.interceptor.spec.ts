c4cbab76e2a74888dcca08e1e37b8b0ac153754c
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-16 07:31:59 +0300

    Added LanguageInterceptor

---
+++ src/app/core/interceptors/language.interceptor.spec.ts
@@ -0,0 +1,49 @@
+import { HttpRequest, provideHttpClient, HttpClient, HttpStatusCode } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { LanguageInterceptor } from './language.interceptor';
+import { TranslateService } from '@ngx-translate/core';
+import { of } from 'rxjs';
+
+describe('LanguageInterceptor', () => {
+    let interceptor: LanguageInterceptor;
+    let requestCloneSpy: jasmine.Spy;
+    let translateServiceSpy: jasmine.SpyObj<TranslateService>;
+
+    const mockUrl: string = 'test-url';
+
+    beforeEach(() => {
+        translateServiceSpy = jasmine.createSpyObj('TranslateService', ['use']);
+        translateServiceSpy.use.and.callThrough();
+
+        TestBed.configureTestingModule({
+            providers: [LanguageInterceptor, provideHttpClient(), provideHttpClientTesting(), HttpClient, { provide: TranslateService, useValue: translateServiceSpy }],
+        });
+
+        interceptor = TestBed.inject(LanguageInterceptor);
+    });
+
+    it('should be created', () => {
+        expect(interceptor).toBeTruthy();
+    });
+
+    it('should add Lang-ISO header on each HTTP request', (done) => {
+        const mockReq = new HttpRequest('GET', mockUrl);
+
+        requestCloneSpy = spyOn(mockReq, 'clone');
+        requestCloneSpy.and.callThrough();
+
+        const next: any = {
+            handle: (req: HttpRequest<any>) => {
+                expect(req.headers.has('Lang-ISO')).toBeTrue();
+                expect(req.headers.get('Lang-ISO')).toBe('en');
+                return of({ status: HttpStatusCode.Ok } as Response);
+            },
+        };
+
+        interceptor.intercept(mockReq, next).subscribe(() => {
+            expect(requestCloneSpy).toHaveBeenCalled();
+            done();
+        });
+    });
+})
