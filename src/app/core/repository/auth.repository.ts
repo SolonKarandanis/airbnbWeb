71ed92835ab81ba0c2811f6a4f8373b4059412f2
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-18 07:09:02 +0300

    Added comments on methods of repositories

--- src/app/core/repository/auth.repository.ts
+++ src/app/core/repository/auth.repository.ts
@@ -13,12 +13,21 @@ import { AUTHENTICATE_REQUEST } from "../guards/SecurityConstants";
 })
 export class AuthRepository extends BaseRepository{
 
+    /**
+    * Submit credentials for login 
+    * @param credentials the credentials submitted
+    * @returns An observable with the JwtDTO
+    */
     login(credentials:SubmitCredentialsDTO):Observable<JwtDTO>{
         return this.http.post<JwtDTO>(`${ApiControllers.AUTH}`,credentials,{
             context: new HttpContext().set(AUTHENTICATE_REQUEST, false),
         })
     }
 
+    /**
+    * Requests the logged in users info 
+    * @returns An observable with the UserModel
+    */
     getUserByToken(): Observable<UserModel> {
         return this.http.get<UserModel>(`${ApiControllers.USERS}/account`);
     }
--- src/app/protected/landlord/repository/landlord.repository.ts
+++ src/app/protected/landlord/repository/landlord.repository.ts
@@ -12,14 +12,29 @@ export class LandlordRepository  extends BaseRepository{
 
     private utilsService = inject(UtilService);
 
+    /**
+    * Get all the Card Listings
+    * @returns An observable with the CardListings
+    */
     public getAllListings():Observable<CardListing[]>{
         return this.http.get<CardListing[]>(`${ApiControllers.LANDLORD}/listings`);
     }
 
+    /**
+    * Delete a specific listing
+    * @param id The listing's ID
+    * @returns An observable that resolves to no data
+    */
     public deleteListing(id:string):Observable<void>{
         return this.http.delete<void>(`${ApiControllers.LANDLORD}/listings/${id}`);
     }
 
+    /**
+    * Create a new Listing 
+    * @param file the file
+    * @param dto the dto
+    * @returns An observable with the created listing
+    */
     public createListing(file: File,dto:string):Observable<CreatedListing>{
         const formData = new FormData();
         formData.append('catalogue', this.utilsService.createJsonBlob(file));
