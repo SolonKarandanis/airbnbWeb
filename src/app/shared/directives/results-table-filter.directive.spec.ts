6d9fb14dc5cd9780a564e7ff6e2ff33e95f83f58
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-21 18:13:45 +0300

    Changes in tests

--- src/app/shared/directives/results-table-filter.directive.spec.ts
+++ src/app/shared/directives/results-table-filter.directive.spec.ts
@@ -1,8 +1,2 @@
 import { ResultsTableFilterDirective } from './results-table-filter.directive';
 
-describe('ResultsTableFilterDirective', () => {
-  // it('should create an instance', () => {
-  //   const directive = new ResultsTableFilterDirective();
-  //   expect(directive).toBeTruthy();
-  // });
-});
--- src/app/shared/directives/results-table-toolbar.directive.spec.ts
+++ src/app/shared/directives/results-table-toolbar.directive.spec.ts
@@ -1,8 +1,2 @@
 import { ResultsTableToolbarDirective } from './results-table-toolbar.directive';
 
-describe('ResultsTableToolbarDirective', () => {
-  // it('should create an instance', () => {
-  //   const directive = new ResultsTableToolbarDirective();
-  //   expect(directive).toBeTruthy();
-  // });
-});
