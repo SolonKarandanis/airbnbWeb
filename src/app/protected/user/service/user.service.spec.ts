1a64aa3e21958c4ca81a697eee092b1dc82d77a8
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-23 13:53:26 +0300

    Implementing UserService  tests

--- src/app/protected/user/service/user.service.spec.ts
+++ src/app/protected/user/service/user.service.spec.ts
@@ -1,12 +1,37 @@
+import { UserStore } from './../store/user.store';
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
 
-xdescribe('UserService', () => {
+type UserStore = InstanceType<typeof UserStore>;
+
+describe('UserService', () => {
   let service: UserService;
+  let userStoreSpy: jasmine.SpyObj<UserStore>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    userStoreSpy = jasmine.createSpyObj('UserStore',[
+      'getUserById',
+      'registerUser',
+      'updateUser',
+      'deleteUser',
+      'activateUser',
+      'deactivateUser',
+      'getUser',
+      'getUserId',
+      'loading',
+      'searchResults',
+      'totalCount'
+    ]);
+
+    TestBed.configureTestingModule({
+      providers:[
+        {
+          provide: UserStore,
+          useValue: userStoreSpy,
+        },
+      ]
+    });
     service = TestBed.inject(UserService);
   });
 
