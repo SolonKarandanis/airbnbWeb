1cfc67279a4020e7bb59a087b108d7df3304320a
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-23 14:27:17 +0300

    Implemenitng TEsts

--- src/app/protected/user/service/user.service.spec.ts
+++ src/app/protected/user/service/user.service.spec.ts
@@ -2,6 +2,7 @@ import { UserStore } from './../store/user.store';
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
+import { mockUpdateUserRequest } from 'src/app/testing/mockData';
 
 type UserStore = InstanceType<typeof UserStore>;
 
@@ -38,4 +39,23 @@ describe('UserService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should execute get user by Id', () =>{
+    const userId: string = '1';
+    service.executeGetUserById(userId);
+
+    expect(userStoreSpy.getUserById).toHaveBeenCalledWith(userId);
+    expect(userStoreSpy.getUserById).toHaveBeenCalledTimes(1);
+  });
+
+  it('should execute update user ', () =>{
+    const userId: string = '1';
+    userStoreSpy.getUserId.and.returnValue(userId);
+    
+    // service.executeUpdateUser(userId);
+
+    expect(userStoreSpy.updateUser).toHaveBeenCalledWith({id:userId,request:mockUpdateUserRequest});
+    expect(userStoreSpy.updateUser).toHaveBeenCalledTimes(1);
+  });
+
 });
