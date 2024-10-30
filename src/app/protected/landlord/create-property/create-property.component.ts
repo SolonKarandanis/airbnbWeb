9360705af43aeaf61e73fcbcd7f5a05e9083850c
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-30 07:29:12 +0200

    Created components

---
+++ src/app/protected/landlord/create-property/create-property.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-property',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      create-property works!
+    </p>
+  `,
+  styles: ``
+})
+export class CreatePropertyComponent {
+
+}
