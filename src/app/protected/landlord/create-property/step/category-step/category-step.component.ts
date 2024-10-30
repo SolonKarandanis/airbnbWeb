e6afe78155a7303992ca5f15920fbf4a8972f5a2
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-30 08:09:39 +0200

    Implemented CategoryStepComponent

--- src/app/protected/landlord/create-property/step/category-step/category-step.component.ts
+++ src/app/protected/landlord/create-property/step/category-step/category-step.component.ts
@@ -1,16 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, input, OnInit, Output } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { Category, CategoryName } from '@models/category.model';
+import { CategoryService } from 'src/app/protected/layout/navbar/category/category.service';
 
 @Component({
   selector: 'app-category-step',
   standalone: true,
-  imports: [],
+  imports: [FaIconComponent],
   template: `
-    <p>
-      category-step works!
-    </p>
+    <div class="categories flex flex-column align-items-center">
+      <h1>Which of these best describe your place?</h1>
+      <div class="flex flex-wrap align-items-center justify-content-center">
+        @for(categoryToDisplay of categories; track categoryToDisplay.technicalName){
+          @if(categoryToDisplay.technicalName !== 'ALL'){
+            <div class="card-step-1 cursor-pointer p-4 m-2 border-round-lg
+              border-200 hover:border-800 flex flex-column align-items-start" 
+              (click)="onSelectCategory(categoryToDisplay.technicalName)"
+              [class.category-selected]="categoryName() == categoryToDisplay.technicalName">
+              <fa-icon class="mb-2" [icon]="categoryToDisplay.icon" size="2x"></fa-icon>
+              <div class="font-bold white-space-nowrap">{{categoryToDisplay.displayName}}</div>
+            </div>
+          }
+        }
+      </div>
+    </div>
   `,
-  styles: ``
+  styles: `
+    @import "../../../../../assets/scss/airbnb-variables";
+
+    .categories {
+      
+      .card-step-1 {
+        min-width: 160px;
+        width: 20%;
+        box-shadow: 0 0 0 1px $border-color;
+      }
+
+      .card-step-1:hover, .category-selected {
+        border-color: transparent;
+        box-shadow: 0 0 0 2px $black;
+      }
+
+      .category-selected {
+        background-color: #F7F7F7;
+      }
+
+    }
+  `
 })
-export class CategoryStepComponent {
+export class CategoryStepComponent implements OnInit{
+
+  private categoryService = inject(CategoryService);
+
+  public categoryName = input.required<CategoryName>();
+
+  @Output()
+  categoryChange = new EventEmitter<CategoryName>();
+
+  @Output()
+  stepValidityChange = new EventEmitter<boolean>();
+
+  public categories: Category[] | undefined;
+
+  ngOnInit(): void {
+    this.categories = this.categoryService.getCategories();
+  }
+
+  onSelectCategory(newCategory: CategoryName): void {
+    this.categoryChange.emit(newCategory);
+    this.stepValidityChange.emit(true);
+  }
 
 }
