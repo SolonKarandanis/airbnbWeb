4585790994f932f1239cb1de13edaa8f015e2acb
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-31 15:32:00 +0200

    Implemented CountryService tests

--- src/app/protected/landlord/service/country.service.spec.ts
+++ src/app/protected/landlord/service/country.service.spec.ts
@@ -1,16 +1,51 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CountryService } from './country.service';
+import { CountryStore } from '@landlord/store/country.store';
 
-xdescribe('CountryService', () => {
+type CountryStore = InstanceType<typeof CountryStore>;
+
+describe('CountryService', () => {
   let service: CountryService;
+  let countryStoreSpy: jasmine.SpyObj<CountryStore>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    countryStoreSpy = jasmine.createSpyObj('CountryStore',[
+      'getAllCountries',
+      'getCountryByCode',
+      'loading',
+      'countries',
+      'selectedCountry',
+    ]);
+
+    TestBed.configureTestingModule({
+      providers:[
+        {
+          provide: CountryStore,
+          useValue: countryStoreSpy,
+        },
+      ]
+    });
     service = TestBed.inject(CountryService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should execute get a country by code', () =>{
+    const code = "test";
+    service.executeGetCountryByCode(code);
+
+    expect(countryStoreSpy.getCountryByCode).toHaveBeenCalledWith(code);
+    expect(countryStoreSpy.getCountryByCode).toHaveBeenCalledTimes(1);
+  });
+
+  it('should execute get all countries', () =>{
+    service.executeGetAllCountries();
+
+    expect(countryStoreSpy.getAllCountries).toHaveBeenCalled()
+    expect(countryStoreSpy.getAllCountries).toHaveBeenCalledTimes(1);
+  });
+
 });
