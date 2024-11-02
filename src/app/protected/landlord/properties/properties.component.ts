0d0b0daf7b70ae78126cb498fcc72f7a4fdd4c54
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-11-02 09:36:03 +0200

    Created PropertiesComponent

---
+++ src/app/protected/landlord/properties/properties.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-properties',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      properties works!
+    </p>
+  `,
+  styles: ``
+})
+export class PropertiesComponent {
+
+}
