37c6c94251d3f49a4a320712d8330674c6d3e969
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-11-28 08:54:15 +0200

    Implemented TenantService tests

--- src/app/protected/tenant/service/tenant.service.spec.ts
+++ src/app/protected/tenant/service/tenant.service.spec.ts
@@ -1,6 +1,7 @@
 import { TenantStore } from "@tenant/store/tenant.store";
 import { TenantService } from "./tenant.service";
 import { TestBed } from "@angular/core/testing";
+import { mockCreateBookingRequest, mockListingSearchRequest, mockPaging } from "src/app/testing/mockData";
 
 type TenantStore = InstanceType<typeof TenantStore>;
 
@@ -42,5 +43,68 @@ describe('TenantService', () => {
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should execute get listings by booking and category', () =>{
+        const pageRequest=mockPaging;
+        const category="ALL";
+        service.executeGetAllListingsByBookingAndCategory(category,pageRequest);
+
+        expect(tenantStoreSpy.getAllListingsByBookingAndCategory).toHaveBeenCalledWith({category,pageRequest});
+        expect(tenantStoreSpy.getAllListingsByBookingAndCategory).toHaveBeenCalledTimes(1);
+    });
+
+    it('should execute search listings', () =>{
+        service.executeSearchListings(mockListingSearchRequest);
+
+        expect(tenantStoreSpy.searchListings).toHaveBeenCalledWith(mockListingSearchRequest);
+        expect(tenantStoreSpy.searchListings).toHaveBeenCalledTimes(1);
+    });
+
+    it('should execute get listing by id', () =>{
+        const id = "1";
+        service.executeGetListingById(id);
+
+        expect(tenantStoreSpy.getListingById).toHaveBeenCalledWith(id);
+        expect(tenantStoreSpy.getListingById).toHaveBeenCalledTimes(1);
+    });
+
+    it('should execute create a booking', () =>{
+        service.executeCreateBooking(mockCreateBookingRequest);
+
+        expect(tenantStoreSpy.createBooking).toHaveBeenCalledWith(mockCreateBookingRequest);
+        expect(tenantStoreSpy.createBooking).toHaveBeenCalledTimes(1);
+    });
+
+    it('should execute check availability', () =>{
+        const id = "1";
+        service.executeCheckAvailability(id);
+
+        expect(tenantStoreSpy.checkAvailability).toHaveBeenCalledWith(id);
+        expect(tenantStoreSpy.checkAvailability).toHaveBeenCalledTimes(1);
+    });
+
+    it('should execute get tenant booked listings', () =>{
+        service.executeGetTenantBookedListings();
+
+        expect(tenantStoreSpy.getTenantBookedListings).toHaveBeenCalled();
+        expect(tenantStoreSpy.getTenantBookedListings).toHaveBeenCalledTimes(1);
+    });
+
+    it('should execute get landlord booked listings', () =>{
+        service.executeGetLandlordBookedListings();
+
+        expect(tenantStoreSpy.getLandlordBookedListings).toHaveBeenCalled();
+        expect(tenantStoreSpy.getLandlordBookedListings).toHaveBeenCalledTimes(1);
+    });
+
+    it('should execute cancel a booking', () =>{
+        const bookingPublicId = "test";
+        const listingPublicId = "test";
+        const byLandlord = false;
+        service.executeCancelBooking(bookingPublicId,listingPublicId,byLandlord);
+
+        expect(tenantStoreSpy.cancelBooking).toHaveBeenCalledWith({bookingPublicId,listingPublicId,byLandlord});
+        expect(tenantStoreSpy.cancelBooking).toHaveBeenCalledTimes(1);
     });
 });
