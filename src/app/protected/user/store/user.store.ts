636b2d3953c3df16a5982483785ac5f126043656
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-01-15 14:08:36 +0200

    Refactored stores

--- src/app/protected/user/store/user.store.ts
+++ src/app/protected/user/store/user.store.ts
@@ -1,4 +1,4 @@
-import { patchState, signalStore, withComputed, withMethods, withState } from "@ngrx/signals";
+import { patchState, signalStore, withComputed, withMethods, withProps, withState } from "@ngrx/signals";
 // import {withDevtools} from "@angular-architects/ngrx-toolkit"
 import { initialUserState, UserState } from "./user.state";
 import { computed, inject } from "@angular/core";
@@ -18,6 +18,11 @@ export const UserStore = signalStore(
     { providedIn: 'root' },
     // withDevtools('user'),
     withState<UserState>(initialUserState),
+    withProps(()=>({
+        userRepo:inject(UserRepository),
+        utilService:inject(UtilService),
+        httpUtil:inject(HttpUtil),
+    })),
     withComputed((
         {
             selectedUser
@@ -63,173 +68,171 @@ export const UserStore = signalStore(
             patchState(state,{loading:false,showError:true,errorMessage:'Error'});
         }
     })),
-    withMethods((
-        state,
-        userRepo = inject(UserRepository),
-        utilService = inject(UtilService),
-        httpUtil = inject(HttpUtil)
-    )=>({
-        searchUsers: rxMethod<UserSearchRequest>(
-            pipe(
-                tap(() => {
-                    state.setLoading(true)
-                }),
-                switchMap((request)=> 
-                    userRepo.searchUsers(request).pipe(
-                        tapResponse({
-                            next:({list,countRows})=>{
-                                state.setSearchResults(list,countRows)
-                            },
-                            error: (error:ErrorResponse) =>{
-                                state.setError(error)
-                            }
-                        })
+    withMethods((state)=>{
+        const {userRepo,httpUtil,utilService} = state;
+        return ({
+            searchUsers: rxMethod<UserSearchRequest>(
+                pipe(
+                    tap(() => {
+                        state.setLoading(true)
+                    }),
+                    switchMap((request)=> 
+                        userRepo.searchUsers(request).pipe(
+                            tapResponse({
+                                next:({list,countRows})=>{
+                                    state.setSearchResults(list,countRows)
+                                },
+                                error: (error:ErrorResponse) =>{
+                                    state.setError(error)
+                                }
+                            })
+                        )
                     )
                 )
-            )
-        ),
-        exportUsersToCsv:rxMethod<UserSearchRequest>(
-            pipe(
-                tap(() => {
-                    state.setLoading(true)
-                }),
-                switchMap((request)=> 
-                    userRepo.exportUsersToCsv(request).pipe(
-                        map((responseData: HttpResponse<ArrayBuffer>) =>({
-                            filename: httpUtil.getFileNameForContentDisposition(responseData.headers),
-                            mimeType: responseData.headers.get('Content-Type')!,
-                            arrayBuffer: responseData.body!,
-                            id: 0, // Just a random number...
-                        })),
-                        tapResponse({
-                            next:(fileData:GenericFile)=>{
-                                utilService.triggerFileDownLoad(fileData.arrayBuffer, fileData.mimeType!, fileData.filename);
-                                state.setLoading(false)
-                            },
-                            error: (error:ErrorResponse) =>{
-                                state.setError(error)
-                            }
-                        })
+            ),
+            exportUsersToCsv:rxMethod<UserSearchRequest>(
+                pipe(
+                    tap(() => {
+                        state.setLoading(true)
+                    }),
+                    switchMap((request)=> 
+                        userRepo.exportUsersToCsv(request).pipe(
+                            map((responseData: HttpResponse<ArrayBuffer>) =>({
+                                filename: httpUtil.getFileNameForContentDisposition(responseData.headers),
+                                mimeType: responseData.headers.get('Content-Type')!,
+                                arrayBuffer: responseData.body!,
+                                id: 0, // Just a random number...
+                            })),
+                            tapResponse({
+                                next:(fileData:GenericFile)=>{
+                                    utilService.triggerFileDownLoad(fileData.arrayBuffer, fileData.mimeType!, fileData.filename);
+                                    state.setLoading(false)
+                                },
+                                error: (error:ErrorResponse) =>{
+                                    state.setError(error)
+                                }
+                            })
+                        )
                     )
                 )
-            )
-        ),
-        getUserById: rxMethod<string>(
-            pipe(
-                tap(() => {
-                    state.setLoading(true)
-                }),
-                switchMap((id)=> 
-                    userRepo.getUserById(id).pipe(
-                        tapResponse({
-                            next:(result)=>{
-                                state.setSelectedUser(result)
-                            },
-                            error: (error:ErrorResponse) =>{
-                                state.setError(error)
-                            }
-                        })
+            ),
+            getUserById: rxMethod<string>(
+                pipe(
+                    tap(() => {
+                        state.setLoading(true)
+                    }),
+                    switchMap((id)=> 
+                        userRepo.getUserById(id).pipe(
+                            tapResponse({
+                                next:(result)=>{
+                                    state.setSelectedUser(result)
+                                },
+                                error: (error:ErrorResponse) =>{
+                                    state.setError(error)
+                                }
+                            })
+                        )
                     )
                 )
-            )
-        ),
-        registerUser: rxMethod<CreateUserRequest>(
-            pipe(
-                tap(() => {
-                    state.setLoading(true)
-                }),
-                switchMap((request)=>
-                    userRepo.registerUser(request).pipe(
-                        tapResponse({
-                            next:(result)=>{
-                                state.setSelectedUser(result)
-                                state.setCreatedUserId(result.publicId);
-                                utilService.showMessage('success','User Successfully Registered');
-                            },
-                            error: (error:ErrorResponse) =>{
-                                state.setError(error)
-                                utilService.showMessage('error',"Couldn't register user");
-                            }
-                        })
+            ),
+            registerUser: rxMethod<CreateUserRequest>(
+                pipe(
+                    tap(() => {
+                        state.setLoading(true)
+                    }),
+                    switchMap((request)=>
+                        userRepo.registerUser(request).pipe(
+                            tapResponse({
+                                next:(result)=>{
+                                    state.setSelectedUser(result)
+                                    state.setCreatedUserId(result.publicId);
+                                    utilService.showMessage('success','User Successfully Registered');
+                                },
+                                error: (error:ErrorResponse) =>{
+                                    state.setError(error)
+                                    utilService.showMessage('error',"Couldn't register user");
+                                }
+                            })
+                        )
                     )
                 )
-            )
-        ),
-        updateUser: rxMethod<{id:string, request:UpdateUserRequest}>(
-            pipe(
-                tap(() => {
-                    state.setLoading(true)
-                }),
-                switchMap(({id,request})=>
-                    userRepo.updateUser(id,request).pipe(
-                        tapResponse({
-                            next:(result)=>{
-                                state.setSelectedUser(result)
-                            },
-                            error: (error:ErrorResponse) =>{
-                                state.setError(error)
-                            }
-                        })
+            ),
+            updateUser: rxMethod<{id:string, request:UpdateUserRequest}>(
+                pipe(
+                    tap(() => {
+                        state.setLoading(true)
+                    }),
+                    switchMap(({id,request})=>
+                        userRepo.updateUser(id,request).pipe(
+                            tapResponse({
+                                next:(result)=>{
+                                    state.setSelectedUser(result)
+                                },
+                                error: (error:ErrorResponse) =>{
+                                    state.setError(error)
+                                }
+                            })
+                        )
                     )
                 )
-            )
-        ),
-        deleteUser: rxMethod<string>(
-            pipe(
-                tap(() => {
-                    state.setLoading(true)
-                }),
-                switchMap((id)=>
-                    userRepo.deleteUser(id).pipe(
-                        tapResponse({
-                            next:(result)=>{
-                                state.setSelectedUser(null)
-                            },
-                            error: (error:ErrorResponse) =>{
-                                state.setError(error)
-                            }
-                        })
+            ),
+            deleteUser: rxMethod<string>(
+                pipe(
+                    tap(() => {
+                        state.setLoading(true)
+                    }),
+                    switchMap((id)=>
+                        userRepo.deleteUser(id).pipe(
+                            tapResponse({
+                                next:(result)=>{
+                                    state.setSelectedUser(null)
+                                },
+                                error: (error:ErrorResponse) =>{
+                                    state.setError(error)
+                                }
+                            })
+                        )
                     )
                 )
-            )
-        ),
-        activateUser: rxMethod<string>(
-            pipe(
-                tap(() => {
-                    state.setLoading(true)
-                }),
-                switchMap((id)=> 
-                    userRepo.activateUser(id).pipe(
-                        tapResponse({
-                            next:(result)=>{
-                                state.setSelectedUser(result)
-                            },
-                            error: (error:ErrorResponse) =>{
-                                state.setError(error)
-                            }
-                        })
+            ),
+            activateUser: rxMethod<string>(
+                pipe(
+                    tap(() => {
+                        state.setLoading(true)
+                    }),
+                    switchMap((id)=> 
+                        userRepo.activateUser(id).pipe(
+                            tapResponse({
+                                next:(result)=>{
+                                    state.setSelectedUser(result)
+                                },
+                                error: (error:ErrorResponse) =>{
+                                    state.setError(error)
+                                }
+                            })
+                        )
                     )
                 )
-            )
-        ),
-        deactivateUser: rxMethod<string>(
-            pipe(
-                tap(() => {
-                    state.setLoading(true)
-                }),
-                switchMap((id)=> 
-                    userRepo.deactivateUser(id).pipe(
-                        tapResponse({
-                            next:(result)=>{
-                                state.setSelectedUser(result)
-                            },
-                            error: (error:ErrorResponse) =>{
-                                state.setError(error)
-                            }
-                        })
+            ),
+            deactivateUser: rxMethod<string>(
+                pipe(
+                    tap(() => {
+                        state.setLoading(true)
+                    }),
+                    switchMap((id)=> 
+                        userRepo.deactivateUser(id).pipe(
+                            tapResponse({
+                                next:(result)=>{
+                                    state.setSelectedUser(result)
+                                },
+                                error: (error:ErrorResponse) =>{
+                                    state.setError(error)
+                                }
+                            })
+                        )
                     )
                 )
-            )
-        ),
-    }))
+            ),
+        })
+    })
 );
